2da7f0bbbd371d4440052b3e25b46e19b3a9ee38
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-25 19:35:43 +0530

    [aditi] Add: Logical problems files added

---
+++ logicalProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logicalProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems");
+        }
+    }
+}
