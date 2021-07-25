7b880439a447e1820c3487f6c6392ec90e694b35
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-25 21:22:21 +0530

    [aditi] Add: Fibonacci Series implemented

---
+++ logicalProblem/FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace logicalProblem
+{
+    class FibonacciSeries
+    {
+        int previousNumber, nextNumber, endNumber, sum;
+        public FibonacciSeries(int number)
+        {
+            this.endNumber = number;
+            this.previousNumber = 0;
+            this.nextNumber = 1;
+            this.sum = 0;
+        }
+        public void Series()
+        {
+            for (int i = 1; i <= endNumber; i++)
+            {
+                Console.Write(previousNumber + " ");
+                sum = previousNumber + nextNumber;
+                previousNumber = nextNumber;
+                nextNumber = sum;
+            }     
+        }
+    }
+    
+}
--- logicalProblem/Program.cs
+++ logicalProblem/Program.cs
@@ -7,6 +7,28 @@ namespace logicalProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Logical Problems");
+            string nextProblem;
+            int choice, number;
+            nextProblem = "y";
+            while (nextProblem.Equals("y"))
+            {
+                Console.WriteLine("1.Fibonacci Series");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Give number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries(number);
+                        fibonacciSeries.Series();
+                        break;
+                    default: 
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue? y or n");
+                nextProblem = Console.ReadLine();
+            }
         }
     }
 }
